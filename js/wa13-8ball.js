d16f3542003a56a46bc94970d170d621c790794d
lillymcnichols <89434826+lillymcnichols@users.noreply.github.com>
2023-04-20 10:38:20 -0600

    wa13

--- js/wa13-8ball.js
+++
@@ -1,9 +0,0 @@
-var slider = document.getElementById("myRange");
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-var sliderOutput = document.querySelector(".slider-output");
-
-
-// Update the current slider value (each time you drag the slider handle)
-function update() {
-  sliderOutput.textContent = slider.value;
-}
